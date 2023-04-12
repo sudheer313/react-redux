0adb6cda6e95af973f6658c4e08a8eb3dc1860a2
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2023-04-13 00:13:19 +1000

    update store.js and todoSlice with actions and reducers

---
+++ src/features/todoSlice.js
@@ -0,0 +1,24 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState: [],
+  reducers: {
+    addTodo: (state, action) => {
+      state.push(action.payload);
+    },
+    removeTodo: (state, action) => {
+      return state.filter((todo) => todo.id !== action.payload);
+    },
+    toggleTodo: (state, action) => {
+      const todo = state.find((todo) => todo.id === action.payload);
+      if (todo) {
+        todo.completed = !todo.completed;
+      }
+    },
+  },
+});
+
+export const { addTodo, removeTodo, toggleTodo } = todoSlice.actions;
+
+export default todoSlice.reducer;
