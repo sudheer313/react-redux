d2613e3c282af53a351cac1d56a1039158f80624
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2023-04-13 10:38:19 +1000

    update store.js to include counterSlice

--- src/store.js
+++ src/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
 import todoReducer from './features/todoSlice';
+import counterReducer from './features/counterSlice';
 
 export default configureStore({
   reducer: {
-    todo: todoReducer
+    todo: todoReducer,
+    counter:counterReducer,
   }
 });
