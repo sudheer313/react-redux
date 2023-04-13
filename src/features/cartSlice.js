10a43dd40ca4774630d719a41895006e03854c53
sudheer313 <103562595+sudheer313@users.noreply.github.com>
2023-04-13 11:48:57 +1000

    update cartSlice.js

---
+++ src/features/cartSlice.js
@@ -0,0 +1,17 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  items: [],
+};
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState,
+  reducers: {
+    addItem: (state, action) => {
+      state.items.push(action.payload);
+    },
+  },
+});
+export const { addItem } = cartSlice.actions;
+export default cartSlice.reducer;
